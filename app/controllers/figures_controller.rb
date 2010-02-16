b752c8d5b5f843b75caeb6dbc68dbfc05fb8557a
Jamis Buck <jamis@37signals.com>
2010-02-16 10:47:18 -0700

    sort figures on index by name

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,6 @@
 class FiguresController < ApplicationController
   def index
-    @figures = Figure.order("updated_at DESC").limit(11)
+    @figures = Figure.order("updated_at DESC").limit(11).sort_by(&:common_name)
     @figures, @more = @figures[0,10], @figures[10]
   end
 
