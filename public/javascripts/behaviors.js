669fe175e40de200e386cd982b5d74dbbbf6139b
Jamis Buck <jamis@37signals.com>
2010-03-01 19:15:47 -0700

    much simpler behaviors implementation

--- public/javascripts/behaviors.js
+++ public/javascripts/behaviors.js
@@ -1,39 +1,8 @@
 var Behaviors = {
-  observers: {},
-
-  observe: function(trigger) {
-    $(document.body).observe(trigger, this.onTrigger.bind(this));
-  },
-
-  add: function(trigger, behavior, callback) {
-    if(!this.observers[trigger]) {
-      this.observe(trigger);
-      this.observers[trigger] = {};
-    }
-
-    if(!this.observers[trigger][behavior]) {
-      this.observers[trigger][behavior] = $A();
-    }
-
-    this.observers[trigger][behavior].push(callback);
-  },
-
-  onTrigger: function(event) {
-    var element = event.findElement("*[data-behaviors]");
-    if(!element) return;
-
-    var behavesLike = element.readAttribute('data-behaviors').split(",");
-    if(behavesLike.length < 1) return;
-
-    var definedBehaviors = this.observers[event.type] || {};
-
-    for(behavior in definedBehaviors) {
-      if(behavesLike.include(behavior)) {
-        definedBehaviors[behavior].each(function(callback) {
-          callback(element, event);
-          if(event.stopped) return;
-        });
-      }
-    }
-  },
+  add: function(trigger, behavior, handler) {
+    $(document.body).observe(trigger, function(event) {
+      var element = event.findElement("*[data-behaviors~=" + behavior + "]");
+      if (element) handler(element, event);
+    });
+  }
 };
